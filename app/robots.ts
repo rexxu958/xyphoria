b5ec1819f5dcb6d81d2ad03f88871c5109e929a7
reymooon <rayhanyassar7@gmail.com>
2026-08-16 08:05:45 +0700

    Update sitemap URL in robots function

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 export default function robots() {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/dashboard", "/api"] }],
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL ?? "https://xyphoria-ii7a6osx9-rexxu958s-projects.vercel.app"}/sitemap.xml`
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL ?? "https://xyphoria.vercel.app"}/sitemap.xml`
   };
 }
